1645a526fe202138b7458beba5a65c9ce6a513d2
Ondosh <tima.tomskihh@gmail.com>
2025-10-23 17:27:24 +0900

    documentation

--- 1 task/calc.cpp
+++ 1 task/calc.cpp
@@ -3,8 +3,14 @@
 #include "calc.hpp"
 
 namespace calculating {
-    /*
-    
+    /**
+    * @brief Функция для вычисления значения по заданной формуле.
+    *
+    * Формула находится по ссылке ниже:
+    * https://ivtipm.github.io/Programming/Glava01/index01.htm#z11
+    * 
+    * @param x, y, z - переменные, задающиеся пользователем.
+    * @return - возвращает результат вычисления по формуле.
     */
     float calculate_a(float x, float y, float z){
         float a;
@@ -12,6 +18,15 @@ namespace calculating {
         return a;
     }
 
+    /**
+    * @brief Функция для вычисления значения по заданной формуле.
+    *
+    * Формула находится по ссылке ниже:
+    * https://ivtipm.github.io/Programming/Glava01/index01.htm#z11
+    * 
+    * @param x, y, z - переменные, задающиеся пользователем.
+    * @return - возвращает результат вычисления по формуле.
+    */
     float calculate_b(float x, float y, float z){
         float b;
         b = (1 + abs(y - x)) + (pow(y - x, 2) / 2) + (pow(abs(y - x), 3) / 3);
