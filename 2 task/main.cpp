bbb345fd22bc1a2898b38a6790e7d4a958b9e9f1
Ondosh <tima.tomskihh@gmail.com>
2025-10-23 21:52:44 +0900

    the program can solve the problem

--- 2 task/main.cpp
+++ 2 task/main.cpp
@@ -6,9 +6,9 @@ int main() {
 
     using namespace std;
     vector<int> nums; 
-
     string user_str = "1";
-    cout << "Вводите нужные числа через Enter. \nЕсли числа закончились, нажмите Enter с пустой строкой";
+    int result = 1;
+    cout << "Вводите нужные числа через Enter. \nЕсли числа закончились, нажмите Enter с пустой строкой\n";
     while (true)
     {
         getline(cin, user_str);
@@ -20,7 +20,10 @@ int main() {
             break;
         }
     }
-    
-    cout << nums.size();
+    for (int num : nums) {
+        result *= num;
+    }
+
+    cout << result;
     return 0;
 }
