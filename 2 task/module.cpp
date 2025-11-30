ff01b361f648a2c4d74d64220fd72705fe9b3dce
Ondosh <tima.tomskihh@gmail.com>
2025-11-30 16:32:55 +0900

    minor changes

--- 2 task/module.cpp
+++ 2 task/module.cpp
@@ -1,10 +1,13 @@
 #include <vector>
 #include "module.hpp"
+#include <cmath>
 
 int mul_vector_nums(const std::vector<int>& nums) {
-    int result = 1;
+    using namespace std;
+    
+    int result = 0;
     for (int num : nums) {
-        result *= num;
+        result += pow(num, 2);
     }
     return result;
 }
