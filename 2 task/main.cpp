00a31cdd2ee91d87b3d1286bcefafeaf3d52f049
Ondosh <tima.tomskihh@gmail.com>
2025-11-30 16:52:07 +0900

    added tests

--- 2 task/main.cpp
+++ 2 task/main.cpp
@@ -2,11 +2,18 @@
 #include <vector>
 #include <cstdlib>  // для rand()
 #include <ctime>    // для time()
+#include <cassert>  // для assert
 #include "module.hpp"
 
 int main() {
     using namespace std;
     using namespace mathfunc;
+
+    assert(sum_of_powers({}) == 0);
+    assert(sum_of_powers({3}) == 9);        // 3^2 = 9
+    assert(sum_of_powers({1, 2, 3}) == 14); // 1^2 + 2^2 + 3^2 = 1 + 4 + 9 = 14
+    assert(sum_of_powers({2, 2}) == 8);     // 4 + 4 = 8
+    assert(sum_of_powers({1, 1, 1, 1}) == 4);
 
     vector<int> nums;
 
